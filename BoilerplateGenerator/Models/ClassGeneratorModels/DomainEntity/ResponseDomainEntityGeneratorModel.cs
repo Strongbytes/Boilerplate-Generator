b3012f23b41fd58613a6b6ff1db35f670070aa8d
Ionuț Dănilă <hello@ionutdanila.com>
2021-06-23 15:48:25 +0300

    Add properties to Generated Domain Entity

--- BoilerplateGenerator/Models/ClassGeneratorModels/DomainEntity/ResponseDomainEntityGeneratorModel.cs
+++ BoilerplateGenerator/Models/ClassGeneratorModels/DomainEntity/ResponseDomainEntityGeneratorModel.cs
@@ -1,6 +1,7 @@
 ﻿using BoilerplateGenerator.Contracts.Services;
 using BoilerplateGenerator.Helpers;
 using BoilerplateGenerator.Models.Enums;
+using BoilerplateGenerator.Models.SyntaxDefinitionModels;
 using BoilerplateGenerator.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,12 @@ namespace BoilerplateGenerator.Models.ClassGeneratorModels.DomainEntity
 {
     public class ResponseDomainEntityGeneratorModel : BaseGenericGeneratorModel
     {
+        private readonly IViewModelBase _viewModelBase;
+
         public ResponseDomainEntityGeneratorModel(IViewModelBase viewModelBase, IMetadataGenerationService metadataGenerationService)
             : base(viewModelBase, metadataGenerationService)
         {
+            _viewModelBase = viewModelBase;
         }
 
         protected override IEnumerable<string> UsingsBuilder => new string[]
@@ -20,5 +24,8 @@ namespace BoilerplateGenerator.Models.ClassGeneratorModels.DomainEntity
         }.Union(base.UsingsBuilder);
 
         public override AssetKind Kind => AssetKind.ResponseDomainEntity;
+
+        protected override IEnumerable<PropertyDefinitionModel> AvailablePropertiesBuilder => _viewModelBase.EntityTree.First()
+                                                                                                            .FilterTreeProperties();
     }
 }
